a5c2dffbaf4abcccbe2f9a877b80a1fa43a46237
Umme Salma Zimia <ummesalmazimia1@gmail.com>
2025-11-23 14:43:30 +0600

    Password Generator

---
+++ Day5/student_height.py
@@ -0,0 +1,28 @@
+# student_heights = input().split()
+# for n in range (0, len(student_heights)):
+#     student_heights[n]= int(student_heights[n])
+
+# sum=0
+# for a in range(0, len(student_heights)):
+#     sum+= student_heights[a]
+#     avg = sum/len(student_heights)
+# print(avg)
+
+#*** higher score ***
+# student_scores = input().split()
+# for n in range (0, len(student_scores)):
+#     student_scores[n]= int(student_scores[n])
+
+
+# high=0
+# for score in student_scores:
+#     if  score > high:
+#         high = score
+        
+        
+# print(high)
+sum=0
+for i in range(2, 53,2):
+ sum+=i
+ 
+print(sum)
